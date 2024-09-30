d8c42da42806e7c77619c1af4d20041628dfdc06
romanbrnv <romanbrnv34.com@gmail.com>
2024-09-30 11:28:07 +0200

    Update tools.py

---
+++ tools.py
@@ -0,0 +1,8 @@
+def ask_for_the_init_number():
+    a = input(int("Saisissez un entier"))
+    return a 
+
+def ask_for_init_base():
+    "" = input("Saisissez la base du nombre choisie")
+    return ""
+
