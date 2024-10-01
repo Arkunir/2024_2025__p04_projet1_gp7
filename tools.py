153b6f0725dca52acfbc3219b58b62a9ea0a8cf5
romanbrnv <romanbrnv34.com@gmail.com>
2024-10-01 09:38:02 +0200

    Rejoute des commandes manquantes

--- tools.py
+++ tools.py
@@ -12,6 +12,22 @@ from data import *
 # def ask_for_the_target_base ():
 #     "" = input("saisissez la base visée")
 #     return ""
+
+ask_for_the_init_number_text = "Par pitié donnes moi un nombre ? :"
+
+ask_again_for_the_init_number_text = "Mauvais nombre, redonne moi un bon ! : "
+
+bin_number_valid_chars = ["0", "1"]
+
+dec_number_valid_chars = \
+    bin_number_valid_chars \
+  + ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+hex_number_valid_chars = \
+    dec_number_valid_chars \
+  + ["A", "B", "C", "D", "E", "F"] \
+  + ["a", "b", "c", "d", "e", "f"]
+
 def check_char_number_validity (char):
     return char in hex_number_valid_chars
 
@@ -20,7 +36,7 @@ def is_a_valid_number (number):
     is_a_valid_char = True
     while is_a_valid_char == True and i <= len (number) - 1:
         is_a_valid_char = check_char_number_validity (number [i])
-        i = i + 1s
+        i = i + 1
     return is_a_valid_char
 
 def ask_for_the_init_number ():
