9f21fddb7ea5b97bd5799adb452682e8a2301a34
romanbrnv <romanbrnv34.com@gmail.com>
2024-10-01 09:26:21 +0200

    amelioration de ask_for_number

--- tools.py
+++ tools.py
@@ -1,17 +1,35 @@
 from data import *
 
 
-def ask_for_the_init_number():
-    a = input(int("Saisissez un entier"))
-    return a 
+# def ask_for_the_init_number():
+#     a = input(int("Saisissez un entier"))
+#     return a 
 
-def ask_for_the_init_base():
-    "" = input("Saisissez la base du nombre choisie (bin, )")
-    return ""
+# def ask_for_the_init_base():
+#     "" = input("Saisissez la base du nombre choisie (bin, )")
+#     return ""
 
-def ask_for_the_target_base ():
-    "" = input("saisissez la base visée")
-    return ""
+# def ask_for_the_target_base ():
+#     "" = input("saisissez la base visée")
+#     return ""
+def check_char_number_validity (char):
+    return char in hex_number_valid_chars
+
+def is_a_valid_number (number):
+    i = 0
+    is_a_valid_char = True
+    while is_a_valid_char == True and i <= len (number) - 1:
+        is_a_valid_char = check_char_number_validity (number [i])
+        i = i + 1s
+    return is_a_valid_char
+
+def ask_for_the_init_number ():
+    init_number = input (ask_for_the_init_number_text)
+    while not (is_a_valid_number (init_number)) == True:
+        init_number = input (ask_again_for_the_init_number_text)
+    return init_number
+        
+ask_for_the_init_number ()
 
 
 
@@ -21,5 +39,12 @@ def bin_dec_hex__to__bin_dec_hex (init_number, init_base, target_base):
         targetnumber = bin_to_decimal (init_number)
     
       
-def bin_to_decimal():
+def bin_to_decimal("binary"):
     
+    decimal = 0
+    for i, bit in enumerate(binaire):
+        if bit == '1':
+            decimal += 2 ** (len(binaire) - 1 - i)
+    return decimal
+    
+print (bin_to_decimal(10))
